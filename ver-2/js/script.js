fcb9059bffe9ea9dc46d5dac0cbaad2a7bc09d00
heyimflh <mufallah06@gmail.com>
2026-01-17 15:00:33 +0700

    Enhance styling and functionality for teachers, reviews, and courses sections
    - Updated SCSS to add new styles for teachers and reviews sections, including hover effects and layout adjustments.
    - Implemented new Swiper instances for teachers, reviews, and logo sliders in JavaScript for improved responsiveness and user interaction.
    - Added accordion functionality for FAQ section to enhance user experience.
    - Removed commented-out width property in the slide class for cleaner code.

--- ver-2/js/script.js
+++ ver-2/js/script.js
@@ -60,3 +60,77 @@ var swiper = new Swiper(".home-courses-slider", {
       },
     });
 
+var swiper = new Swiper(".teachers-slider", {
+      loop:true,
+      grabCursor:true,
+      spaceBetween: 20,
+      breakpoints: {
+        0: {
+          slidesPerView: 1,
+        },
+        768: {
+          slidesPerView: 2,
+        },
+        991: {
+          slidesPerView: 3,
+        },
+      },
+    });
+
+var swiper = new Swiper(".reviews-slider", {
+      loop:true,
+      grabCursor:true,
+      spaceBetween: 20,
+      breakpoints: {
+        0: {
+          slidesPerView: 1,
+        },
+        768: {
+          slidesPerView: 2,
+        },
+        991: {
+          slidesPerView: 3,
+        },
+      },
+    });
+
+var swiper = new Swiper(".logo-slider", {
+      loop:true,
+      grabCursor:true,
+      spaceBetween: 20,
+      breakpoints: {
+        0: {
+          slidesPerView: 1,
+        },
+        450: {
+          slidesPerView: 2,
+        },
+        768: {
+          slidesPerView: 3,
+        },
+        991: {
+          slidesPerView: 4,
+        },
+        1200: {
+          slidesPerView: 5,
+        },
+      },
+    });
+
+    
+    let accordion = document.querySelectorAll('.faq .accordion-container .accordion');  
+    
+    accordion.forEach(acco =>{
+      acco.onclick = () =>{
+        accordion.forEach(dion => dion.classList.remove('active'));
+        acco.classList.toggle('active');
+      };
+    });
+    
+    document.querySelector('.load-more .btn').onclick = () =>{
+      document.querySelectorAll('.courses .box-container .hide').forEach(show =>{
+        show.style.display = 'block';
+      });
+      document.querySelector('.load-more .btn').style.display = 'none';
+    };
+
